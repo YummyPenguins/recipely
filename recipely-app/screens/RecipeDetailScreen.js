76a44a2e1ef49a88fc0642f48a94f57efbde4787
Kevin Nguyen <knguy47@gmail.com>
2017-05-19 01:50:12 -0500

    Change all fetch urls to yummy-penguin deployed url

--- recipely-app/screens/RecipeDetailScreen.js
+++ recipely-app/screens/RecipeDetailScreen.js
@@ -28,7 +28,7 @@ class RecipeDetailScreen extends Component {
     const { recipe_id } = this.props.navigation.state.params;
     // Fetch ingredients if no ingredients were passed down
     if (!this.state.ingredients) {
-      fetch(`https://jellyfiish-recipely.herokuapp.com/api/recipes/${recipe_id}`)
+      fetch(`https://yummypenguin-recipely.herokuapp.com/api/recipes/${recipe_id}`)
         .then(res => res.json())
         .then(result => this.setState({ ingredients: result.recipe.ingredients }));
     }
@@ -76,7 +76,7 @@ class RecipeDetailScreen extends Component {
     });
     // Remove note from database
     const { idToken } = this.props.navigation.state.params;
-    fetch(`https://jellyfiish-recipely.herokuapp.com/api/notes/${note.id}`, {
+    fetch(`https://yummypenguin-recipely.herokuapp.com/api/notes/${note.id}`, {
       method: 'DELETE',
       headers: {
         'x-access-token': `Bearer ${idToken}`,
