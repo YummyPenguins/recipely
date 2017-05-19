f3a735275a9c35c330fcd1064731373074842a68
David Hsiao <dhsiao89@gmail.com>
2017-05-19 00:12:10 -0700

    Add Shopping List tab and rearrange exisitng tabs

--- recipely-app/navigation/routes.js
+++ recipely-app/navigation/routes.js
@@ -1,193 +1,215 @@
-import { Constants } from 'expo';
-import React, { Component } from 'react';
-import { View } from 'react-native';
-import { MaterialIcons } from '@expo/vector-icons';
+import { Constants } from "expo";
+import React, { Component } from "react";
+import { View } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
 import {
   DrawerNavigator,
   DrawerItems,
   StackNavigator,
   TabNavigator
-} from 'react-navigation';
-import SplashScreen from '../screens/SplashScreen';
-import AuthScreen from '../screens/AuthScreen';
-import PhotoScreen from '../screens/PhotoScreen';
-import PhotoResultScreen from '../screens/PhotoResultScreen';
-import RecipeScreen from '../screens/RecipeScreen';
-import RecipeDetailScreen from '../screens/RecipeDetailScreen';
-import EditNoteScreen from '../screens/EditNoteScreen';
-import AddNoteScreen from '../screens/AddNoteScreen';
-import PopularScreen from '../screens/PopularScreen';
-import SearchScreen from '../screens/SearchScreen';
-import SearchResultScreen from '../screens/SearchResultScreen';
-import SearchDetailScreen from '../screens/SearchDetailScreen';
-import NoteScreen from '../screens/NoteScreen';
-import ProfileScreen from '../screens/ProfileScreen';
-import SideDrawer from '../components/SideDrawer.js';
+} from "react-navigation";
+import SplashScreen from "../screens/SplashScreen";
+import AuthScreen from "../screens/AuthScreen";
+import PhotoScreen from "../screens/PhotoScreen";
+import PhotoResultScreen from "../screens/PhotoResultScreen";
+import RecipeScreen from "../screens/RecipeScreen";
+import RecipeDetailScreen from "../screens/RecipeDetailScreen";
+import EditNoteScreen from "../screens/EditNoteScreen";
+import AddNoteScreen from "../screens/AddNoteScreen";
+import PopularScreen from "../screens/PopularScreen";
+import SearchScreen from "../screens/SearchScreen";
+import SearchResultScreen from "../screens/SearchResultScreen";
+import SearchDetailScreen from "../screens/SearchDetailScreen";
+import NoteScreen from "../screens/NoteScreen";
+import ProfileScreen from "../screens/ProfileScreen";
+import SideDrawer from "../components/SideDrawer.js";
 
-const MenuIcon = (navigation) => (
+const MenuIcon = navigation => (
   <MaterialIcons
     name="menu"
     size={30}
-    style={{paddingLeft: 12, color: '#777'}}
-    onPress={() => navigation.navigate('DrawerOpen')}
+    style={{ paddingLeft: 12, color: "#777" }}
+    onPress={() => navigation.navigate("DrawerOpen")}
   />
 );
 
-const DrawerIcon = (name) => ({ tintColor }) => (
+const DrawerIcon = name => ({ tintColor }) => (
   <MaterialIcons name={name} size={24} color={tintColor} />
 );
 
-const TabBarIcon = (name) => ({ tintColor }) => (
+const TabBarIcon = name => ({ tintColor }) => (
   <MaterialIcons name={name} size={24} color={tintColor} />
 );
 
-const RecipeStack = StackNavigator({
-  Recipes: {
-    screen: RecipeScreen,
-    navigationOptions: ({ navigation }) => ({
-      title: 'Saved Recipes',
-      headerLeft: MenuIcon(navigation),
-    }),
+const RecipeStack = StackNavigator(
+  {
+    Recipes: {
+      screen: RecipeScreen,
+      navigationOptions: ({ navigation }) => ({
+        title: "Saved Recipes",
+        headerLeft: MenuIcon(navigation)
+      })
+    },
+    Details: {
+      screen: RecipeDetailScreen,
+      navigationOptions: ({ navigation }) => ({
+        title: `${navigation.state.params.title}`
+      })
+    },
+    EditNote: {
+      screen: EditNoteScreen,
+      navigationOptions: ({ navigation }) => ({
+        title: "Edit Note"
+      })
+    },
+    AddNote: {
+      screen: AddNoteScreen,
+      navigationOptions: ({ navigation }) => ({
+        title: `Add Note: ${navigation.state.params.title}`
+      })
+    }
   },
-  Details: {
-    screen: RecipeDetailScreen,
-    navigationOptions: ({ navigation }) => ({
-      title: `${navigation.state.params.title}`,
-    }),
-  },
-  EditNote: {
-    screen: EditNoteScreen,
-    navigationOptions: ({ navigation }) => ({
-      title: 'Edit Note',
-    }),
-  },
-  AddNote: {
-    screen: AddNoteScreen,
-    navigationOptions: ({navigation }) => ({
-      title: `Add Note: ${navigation.state.params.title}`
-    }),
-  },
-}, {
-  cardStyle: {
-    paddingTop: Constants.statusBarHeight,
+  {
+    cardStyle: {
+      paddingTop: Constants.statusBarHeight
+    }
   }
-});
+);
 
-const NoteStack = StackNavigator({
-  Notes: {
-    screen: NoteScreen,
-    navigationOptions: ({navigation}) => ({
-      title: 'All Notes',
-      headerLeft: MenuIcon(navigation),
-    }),
+const NoteStack = StackNavigator(
+  {
+    Notes: {
+      screen: NoteScreen,
+      navigationOptions: ({ navigation }) => ({
+        title: "All Notes",
+        headerLeft: MenuIcon(navigation)
+      })
+    }
   },
-}, {
-  cardStyle: {
-    paddingTop: Constants.statusBarHeight,
+  {
+    cardStyle: {
+      paddingTop: Constants.statusBarHeight
+    }
   }
-});
+);
 
 const SearchTab = TabNavigator({
   Photo: {
     screen: PhotoScreen,
     navigationOptions: ({ navigation }) => ({
-      tabBarLabel: 'Photo',
-      tabBarIcon: TabBarIcon('photo-camera'),
-    }),
-  },
-  Search: {
-    screen: SearchScreen,
-    navigationOptions: ({ navigation }) => ({
-      tabBarLabel: 'Ingredients',
-      tabBarIcon: TabBarIcon('search'),
-    }),
+      tabBarLabel: "Photo",
+      tabBarIcon: TabBarIcon("photo-camera")
+    })
   },
   Popular: {
     screen: PopularScreen,
     navigationOptions: ({ navigation }) => ({
-      tabBarLabel: 'Popular',
-      tabBarIcon: TabBarIcon('whatshot'),
-    }),
+      tabBarLabel: "Popular",
+      tabBarIcon: TabBarIcon("whatshot")
+    })
   },
+  Search: {
+    screen: SearchScreen,
+    navigationOptions: ({ navigation }) => ({
+      tabBarLabel: "Ingredients",
+      tabBarIcon: TabBarIcon("search")
+    })
+  },
+  ShoppingList: {
+    screen: PhotoScreen,
+    navigationOptions: ({ navigation }) => ({
+      tabBarLabel: "Shopping List",
+      tabBarIcon: TabBarIcon("photo-camera")
+    })
+  }
 });
 
 // Create a title for tab navigator.
 SearchTab.navigationOptions = ({ navigation }) => {
   return {
-    title: 'Find recipes',
-    headerLeft: MenuIcon(navigation),
-  }
+    title: "Find recipes",
+    headerLeft: MenuIcon(navigation)
+  };
 };
 
-const SearchStack = StackNavigator({
-  SearchHome: {
-    screen: SearchTab,
+const SearchStack = StackNavigator(
+  {
+    SearchHome: {
+      screen: SearchTab
+    },
+    SearchResult: {
+      screen: SearchResultScreen,
+      navigationOptions: ({ navigation }) => ({
+        title: "Search results"
+      })
+    },
+    SearchDetail: {
+      screen: SearchDetailScreen,
+      navigationOptions: ({ navigation }) => ({
+        title: `${navigation.state.params.title}`
+      })
+    }
   },
-  SearchResult: {
-    screen: SearchResultScreen,
-    navigationOptions: ({ navigation }) => ({
-      title: 'Search results',
-    }),
-  },
-  SearchDetail: {
-    screen: SearchDetailScreen,
-    navigationOptions: ({ navigation }) => ({
-      title: `${navigation.state.params.title}`,
-    }),
-  },
-}, {
-  cardStyle: {
-    paddingTop: Constants.statusBarHeight,
+  {
+    cardStyle: {
+      paddingTop: Constants.statusBarHeight
+    }
   }
-});
+);
 
-const ProfileStack = StackNavigator({
-  ProfileHome: {
-    screen: ProfileScreen,
-    navigationOptions: ({ screenProps, navigation }) => ({
-      title: `${screenProps.user.username}'s profile`,
-      headerLeft: MenuIcon(navigation),
-    }),
+const ProfileStack = StackNavigator(
+  {
+    ProfileHome: {
+      screen: ProfileScreen,
+      navigationOptions: ({ screenProps, navigation }) => ({
+        title: `${screenProps.user.username}'s profile`,
+        headerLeft: MenuIcon(navigation)
+      })
+    }
+  },
+  {
+    cardStyle: {
+      paddingTop: Constants.statusBarHeight
+    }
   }
-}, {
-  cardStyle: {
-    paddingTop: Constants.statusBarHeight,
-  }
-});
+);
 
-const MainDrawerNavigator = DrawerNavigator({
-  Find: {
-    screen: SearchStack,
-    navigationOptions: ({navigation}) => ({
-      drawerLabel: 'Find recipes',
-      drawerIcon: DrawerIcon('search'),
-    }),
+const MainDrawerNavigator = DrawerNavigator(
+  {
+    Find: {
+      screen: SearchStack,
+      navigationOptions: ({ navigation }) => ({
+        drawerLabel: "Find recipes",
+        drawerIcon: DrawerIcon("search")
+      })
+    },
+    RecipeStack: {
+      screen: RecipeStack,
+      navigationOptions: ({ navigation }) => ({
+        drawerLabel: "View saved recipes",
+        drawerIcon: DrawerIcon("list")
+      })
+    },
+    Notes: {
+      screen: NoteStack,
+      navigationOptions: ({ navigation }) => ({
+        drawerLabel: "View notes",
+        drawerIcon: DrawerIcon("note")
+      })
+    },
+    Profile: {
+      screen: ProfileStack,
+      navigationOptions: ({ navigation }) => ({
+        drawerLabel: "Profile",
+        drawerIcon: DrawerIcon("account-circle")
+      })
+    }
   },
-  RecipeStack: {
-    screen: RecipeStack,
-    navigationOptions: ({navigation}) => ({
-      drawerLabel: 'View saved recipes',
-      drawerIcon: DrawerIcon('list'),
-    }),
-  },
-  Notes: {
-    screen: NoteStack,
-    navigationOptions: ({navigation}) => ({
-      drawerLabel: 'View notes',
-      drawerIcon: DrawerIcon('note'),
-    }),
-  },
-  Profile: {
-    screen: ProfileStack,
-    navigationOptions: ({navigation}) => ({
-      drawerLabel: 'Profile',
-      drawerIcon: DrawerIcon('account-circle'),
-    }),
-  },
-}, {
-  // Custom side menu. See '../components/SideDrawer.js'.
-  contentComponent: props => SideDrawer(props),
-});
+  {
+    // Custom side menu. See '../components/SideDrawer.js'.
+    contentComponent: props => SideDrawer(props)
+  }
+);
 
 const StartupStack = StackNavigator(
   {
@@ -195,9 +217,11 @@ const StartupStack = StackNavigator(
     AuthScreen: { screen: AuthScreen },
     MainDrawerNavigator: {
       screen: ({ screenProps, navigation }) => (
-        <MainDrawerNavigator screenProps={{ ...screenProps, rootNavigation: navigation }} />
+        <MainDrawerNavigator
+          screenProps={{ ...screenProps, rootNavigation: navigation }}
+        />
       )
-    },
+    }
   },
   {
     navigationOptions: {
