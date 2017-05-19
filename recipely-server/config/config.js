c2294fe9e2a20ffcf0d5eb5ad5087cb6634ea2ad
David Hsiao <dhsiao89@gmail.com>
2017-05-18 19:17:34 -0700

    remove config from gitignore. add config file

---
+++ recipely-server/config/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  // Food2Fork API key
+  F2F_API_KEY: "ac96cb5d19a3078bc664338a55e35cf9",
+  // URL to your local postgres server (e.g. "postgres://user:password@localhost:5432/mydatabase")
+  DATABASE_URL: "postgres://postgres:security1@localhost:5432/postgres",
+  // Secret string used to encode the user authentication tokens
+  TOKEN_SECRET: "HRR23D",
+  // Clarifai API client id
+  CLIENT_ID: "uMHAXxFbXKM9_mFJ9nWixro6w3fyJo-3vUtqdzE-",
+  // Clarifai API client secret
+  CLIENT_SECRET: "KptG3CqgvNIWaYUE9JNeug9MHZQMNdcNpICPZKJq"
+};
