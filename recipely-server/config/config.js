7d984b291c34284fb7446663812e570a572f66ff
David Hsiao <dhsiao89@gmail.com>
2017-05-18 22:03:29 -0700

    Remove API from config for deployment

--- recipely-server/config/config.js
+++ recipely-server/config/config.js
@@ -1,13 +1,13 @@
 module.exports = {
   // Food2Fork API key
-  F2F_API_KEY: "ac96cb5d19a3078bc664338a55e35cf9",
+  // F2F_API_KEY: "ac96cb5d19a3078bc664338a55e35cf9",
   // URL to your local postgres server (e.g. "postgres://user:password@localhost:5432/mydatabase")
   DATABASE_URL: "postgresql-adjacent-56744",
   //"postgres://postgres:security1@localhost:5432/postgres",
   // Secret string used to encode the user authentication tokens
-  TOKEN_SECRET: "HRR23D",
+  // TOKEN_SECRET: "HRR23D",
   // Clarifai API client id
-  CLIENT_ID: "uMHAXxFbXKM9_mFJ9nWixro6w3fyJo-3vUtqdzE-",
+  // CLIENT_ID: "uMHAXxFbXKM9_mFJ9nWixro6w3fyJo-3vUtqdzE-",
   // Clarifai API client secret
-  CLIENT_SECRET: "KptG3CqgvNIWaYUE9JNeug9MHZQMNdcNpICPZKJq"
+  // CLIENT_SECRET: "KptG3CqgvNIWaYUE9JNeug9MHZQMNdcNpICPZKJq"
 };
